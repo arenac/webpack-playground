c70e470ea37115c81543c6b31e6543e9a3916626
Nilo Neregato <neregato.nilo@gmail.com>
2020-05-24 20:37:15 +0200

    Inject js and css:
    Durgin building, the js scripts and  the css will be injectected into the index.html which is part or the src folder and will be part or the dist folder.
    
    Copy the images to the dist folder when building for production.
    
    Lastly, clean the dist folder after each build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/js/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/dist'
+    // publicPath: '/dist'
   },
   module: {
     rules: [
@@ -35,12 +37,35 @@ module.exports = {
           'css-loader',
           'sass-loader'
         ]
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'imgs/',
+              publicPath: 'imgs/'
+            }
+          }
+        ]
       }
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'main.css'
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['dist/*']
     })
   ]
 };
